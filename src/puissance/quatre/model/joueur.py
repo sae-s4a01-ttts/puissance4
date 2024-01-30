a246a0ee745f4a6abc460514d6fed5487102c5b7
Thomas Lemaire <thomas.lemaire@iut-rodez.fr>
2024-01-30 16:22:49 +0100

    Ajout des commentaires

--- src/puissance/quatre/model/joueur.py
+++ src/puissance/quatre/model/joueur.py
@@ -3,10 +3,12 @@ class Joueur:
     self.__nom = ""
 
     def __init__(self, nom):
-        self.__nom = nom
+        self.setNom(nom)
     
     def setNom(self, nom:str):
+        """ @param nom la nouvelle valeur de self.__nom """
         self.__nom = nom
 
     def getNom(self) -> str:
+        """ @return le nom du joueur """
         return self.__nom
