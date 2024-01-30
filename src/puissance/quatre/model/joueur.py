1123cdfe04337e3f207b1daea1faa819355736ea
Thomas Lemaire <thomas.lemaire@iut-rodez.fr>
2024-01-30 19:01:51 +0100

    Refactoring de la classe Joueur

--- src/puissance/quatre/model/joueur.py
+++ src/puissance/quatre/model/joueur.py
@@ -1,14 +1,18 @@
 class Joueur:
 
-    self.__nom = ""
+    __nom = ""
 
     def __init__(self, nom):
         self.setNom(nom)
     
     def setNom(self, nom:str):
         """ @param nom la nouvelle valeur de self.__nom """
-        self.__nom = nom
+        if self.__isValid(nom) : self.__nom = nom
+        else: raise ValueError("Le nom du joueur n'est pas valide")
 
     def getNom(self) -> str:
         """ @return le nom du joueur """
         return self.__nom
+    
+    def __isValid(self, nom:str) -> bool:
+        return nom != None and nom != ""
