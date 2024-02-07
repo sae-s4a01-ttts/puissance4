f8564e201dc927c6d43a63aa9cb35892440b0363
Thomas Lemaire <thomas.lemaire@iut-rodez.fr>
2024-02-07 21:20:14 +0100

    Refactoring & Hashage version auto player

--- src/puissance/quatre/model/grid.py
+++ src/puissance/quatre/model/grid.py
@@ -7,10 +7,12 @@ class Grid:
     __WIDTH:int  = 7
     
     __grid:list[Column]
+    __next_player:int
     __hashcode:hex
 
     def __init__(self) -> None:
         self.__generate_grid()
+        self.__next_player = 0
 
     def __generate_grid(self) -> bool:
         grid_with_columns = [Column() for x in range(0, self.__WIDTH)]
@@ -26,9 +28,10 @@ class Grid:
         
         return True
     
-    def play_column(self, index:int, player:int) -> bool:
+    def play_column(self, index:int) -> bool:
         if not (index > -1 and index < 7): return False
-        self.__grid[index].play_cell(player)
+        self.__grid[index].play_cell(self.__next_player)
+        self.__next_player = (self.__next_player + 1) % 2
         self.__generate_hashcode()
         return True
     
@@ -44,8 +47,9 @@ class Grid:
     
 g = Grid()
 
-g.play_column(1,0)
-g.play_column(6,1)
+g.play_column(1)
+g.play_column(6)
+g.play_column(3)
 
 ghash = g.get_hashcode()
 print(ghash)
